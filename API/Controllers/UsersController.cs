ae9950cadebbd625e94fc3092cc3c21ec29342bf
Lam Nguyen <l0nguy10@louisville.edu>
2021-08-31 21:30:01 -0400

    first commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    
+    public class UsersController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUser()
+        {
+            return  await _context.Users.ToListAsync();
+
+        }
+        [HttpGet("{id}")]
+        [Authorize]
+        public async Task<ActionResult<AppUser>> GetUserAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+
+        }
+    }
+}
